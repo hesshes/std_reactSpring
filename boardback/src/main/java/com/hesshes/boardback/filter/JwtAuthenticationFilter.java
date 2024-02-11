ee602e9a61a3990346ff09d9cfc8f32d18cff32d
ktaeu <hesshes@naver.com>
2024-02-11 23:44:04 +0900

    security filter chain

--- boardback/src/main/java/com/hesshes/boardback/filter/JwtAuthenticationFilter.java
+++ boardback/src/main/java/com/hesshes/boardback/filter/JwtAuthenticationFilter.java
@@ -2,16 +2,24 @@ package com.hesshes.boardback.filter;
 
 import java.io.IOException;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.hesshes.boardback.provider.JwtProvider;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -24,10 +32,44 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        try {
+
+            String token = parseBearerToken(request);
+
+            if (token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String email = jwtProvider.validate(token);
+
+            if (email == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, null,
+                    AuthorityUtils.NO_AUTHORITIES);
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authenticationToken);
+
+            SecurityContextHolder.setContext(securityContext);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        filterChain.doFilter(request, response);
+
     }
 
-    private String parseBearerToken(HttpServletRequest req) {
-        String autorization = req.getHeader("Authorization");
+    private String parseBearerToken(HttpServletRequest request) {
+        String autorization = request.getHeader("Authorization");
 
         boolean hasAuthorization = StringUtils.hasText(autorization);
 
