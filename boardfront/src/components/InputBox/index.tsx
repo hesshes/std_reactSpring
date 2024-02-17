e1807f4c6782ebe08362f4e9193dc40e89d7cfac
ktaeu <hesshes@naver.com>
2024-02-18 01:11:49 +0900

    msi 20240218 am 01:11

--- boardfront/src/components/InputBox/index.tsx
+++ boardfront/src/components/InputBox/index.tsx
@@ -1,21 +1,59 @@
-import { forwardRef } from "react";
+import { ChangeEvent, Dispatch, SetStateAction, forwardRef } from "react";
 import "./style.css";
 
-interface Props {}
+// interface : Input Box Component Properties
+interface Props {
+    label: string;
+    type: "text" | "password";
+    placeholder: string;
+    value: string;
+    setValue: Dispatch<SetStateAction<string>>;
+    error: boolean;
+
+    icon?: string;
+    onButtonClick?: () => void;
+    message?: string;
+}
 // component : Input Box
 const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+    // state : properties //
+    const { label, type, placeholder, value, error } = props;
+    const { icon, message, onButtonClick } = props;
+    const { setValue } = props;
+
+    // event handler //
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        //const value = event.target.value;
+        const { value } = event.target;
+        setValue(value);
+    };
     return (
         <div className="inputbox">
-            <div className="inputbox-label">{"비밀번호"}</div>
-            <div className="inputbox-container">
-                <input className="input" />
-                <div className="icon-button">
-                    <div className=""></div>
-                </div>
+            <div className="inputbox-label">{label}</div>
+            <div
+                className={
+                    error ? "inputbox-container-error" : "inputbox-container"
+                }
+            >
+                <input
+                    ref={ref}
+                    type={type}
+                    className="input"
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={onChangeHandler}
+                />
+                {onButtonClick !== undefined && (
+                    <div className="icon-button">
+                        {icon !== undefined && (
+                            <div className={`icon ${icon}`}></div>
+                        )}
+                    </div>
+                )}
             </div>
-            <div className="inputbox-message">
-                {"비밀번호는 8자 이상 입력해주세요."}
-            </div>
+            {message !== undefined && (
+                <div className="inputbox-message">{message}</div>
+            )}
         </div>
     );
 });
