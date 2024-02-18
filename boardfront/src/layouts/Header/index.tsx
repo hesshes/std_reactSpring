cf4c9edfc5208a92d53487fd0be054bae051761a
ktaeu <hesshes@naver.com>
2024-02-18 22:06:49 +0900

    msi 20240218 pm 10:06

--- boardfront/src/layouts/Header/index.tsx
+++ boardfront/src/layouts/Header/index.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {
+    ChangeEvent,
+    KeyboardEvent,
+    useEffect,
+    useRef,
+    useState,
+} from "react";
 import "./style.css";
-import { useNavigate } from "react-router-dom";
-import { MAIN_PATH } from "constant";
+import { useNavigate, useParams } from "react-router-dom";
+import { MAIN_PATH, SEARCH_PATH } from "constant";
 
 // Component : 헤더 레이아웃 //
 export default function Header() {
@@ -15,10 +21,78 @@ export default function Header() {
 
     // Component: 검색 컴포넌트 //
     const SearchButton = () => {
-        // render: 검색 버튼 랜더링 //
+        // state : 검색어 버튼 상태 //
+        const searchButtonRef = useRef<HTMLInputElement | null>(null);
+        // state : 검색 버튼 상태 //
+        const [status, setStatus] = useState<boolean>(false);
+
+        // state : 검색어 상태 //
+        const [word, setWord] = useState<string>("");
+
+        // state : 검색어 path variable 상태 //
+        const { searchWord } = useParams();
+
+        // event handler: 검색어 변경 처리 함수 //
+        const onSearchWordChangeHandler = (
+            event: ChangeEvent<HTMLInputElement>
+        ) => {
+            const value = event.target.value;
+            setWord(value);
+        };
+
+        // event handler: 검색어 키 엔터 처리 함수  //
+        const onSearchWordKeyDownHandler = (
+            event: KeyboardEvent<HTMLInputElement>
+        ) => {
+            if (event.key !== "Enter" || !searchButtonRef.current) return;
+            //if(!searchButtonRef) return;
+            searchButtonRef.current.click();
+        };
+
+        // event handler: 기본 검색 아이콘 클릭 처리 함수 //
+        const onSearchButtonClickHandler = () => {
+            if (!status) {
+                setStatus(!status);
+                return;
+            }
+            navigate(SEARCH_PATH(word));
+        };
+
+        // effect : 검색어 path variable 변경 될 때마다 실행될 함수 //
+        useEffect(() => {
+            if (searchWord) {
+                setWord(searchWord);
+                setStatus(true);
+            }
+        }, [searchWord]);
+        if (!status)
+            // render: 검색 버튼 랜더링 status : false//
+            return (
+                <div
+                    className="icon-button"
+                    onClick={onSearchButtonClickHandler}
+                >
+                    <div className="icon search-light-icon"></div>
+                </div>
+            );
+        // render: 검색 버튼 랜더링 status : true//
         return (
-            <div className="icon-button">
-                <div className="icon search-light-icon"></div>
+            <div className="header-search-input-box">
+                <input
+                    className="header-search-input"
+                    type="text"
+                    placeholder="검색어를 입력해주세요."
+                    value={word}
+                    onChange={onSearchWordChangeHandler}
+                    onKeyDown={onSearchWordKeyDownHandler}
+                />
+                <div
+                    ref={searchButtonRef}
+                    className="icon-button"
+                    onClick={onSearchButtonClickHandler}
+                >
+                    <div className="icon search-light-icon"></div>
+                </div>
             </div>
         );
     };
