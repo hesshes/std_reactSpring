cd9a5234a1f084656caa2e51402a255fd842b4d3
ktaeu <hesshes@naver.com>
2024-02-24 23:45:40 +0900

    20240224 pm 11:45 msi

--- boardback/src/main/java/com/hesshes/boardback/entity/UserEntity.java
+++ boardback/src/main/java/com/hesshes/boardback/entity/UserEntity.java
@@ -25,15 +25,15 @@ public class UserEntity {
     private String address;
     private String addressDetail;
     private String profileImage;
+    private boolean agreedPersonal;
 
-    public userEntity(SignUpRequestDto dto){
-        this.email;
-        private String password;
-        private String nickname;
-        private String telNumber;
-        private String address;
-        private String addressDetail;
-        private String profileImage;
-    
+    public UserEntity(SignUpRequestDto dto){
+        this.email = dto.getEmail();
+        this.password = dto.getPassword();
+        this.nickname = dto.getNickname();
+        this.telNumber = dto.getTelNumber();
+        this.address = dto.getAddress();
+        this.addressDetail = dto.getAddressDetail();
+        this.agreedPersonal = dto.g
     }
 }
