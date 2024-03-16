efd43b92d62cd87161f0a01ca9c7b229b5c11581
ktaeu <hesshes@naver.com>
2024-03-16 11:40:01 +0900

    2024-03-16 am 11:39 asus

--- boardfront/src/views/Board/Write/write.tsx
+++ boardfront/src/views/Board/Write/write.tsx
@@ -3,6 +3,7 @@ import "./style.css";
 import { useBoardStore, useLoginUserStore } from "stores";
 import { useNavigate } from "react-router-dom";
 import { MAIN_PATH } from "constant";
+import { useCookies } from "react-cookie";
 
 // Component 게시물 작성 화면 //
 export default function BoardWrite() {
@@ -20,8 +21,8 @@ export default function BoardWrite() {
     const { boardImageFileList, setBoardImageFileList } = useBoardStore();
     const { resetBoard } = useBoardStore();
 
-    // state : 로그인 유저 상태 //
-    const { loginUser } = useLoginUserStore();
+    // state : 쿠키 상태 //
+    const [cookies, setCookies] = useCookies();
 
     // state : 게시물 이미지 미리보기 URL 상태 //
     const [imageUrls, setImageUrls] = useState<string[]>([]);
@@ -85,7 +86,8 @@ export default function BoardWrite() {
 
     // effect : 마운트 시 실행할 함수 //
     useEffect(() => {
-        if (!loginUser) {
+        const accessToken = cookies.accessToken;
+        if (!accessToken) {
             navigate(MAIN_PATH());
             return;
         }
