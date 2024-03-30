d052a7beb7c4b6c126d6ff57266bf22b5a53b7bd
ktaeu <hesshes@naver.com>
2024-03-31 01:07:59 +0900

    2024-3-31 am : 01:07

--- boardfront/src/views/Board/Detail/index.tsx
+++ boardfront/src/views/Board/Detail/index.tsx
@@ -24,9 +24,7 @@ export default function BoardDetail() {
             <div id="board-detail-top">
                 <div className="board-detail-top-header">
                     <div className="board-detail-title">
-                        {
-                            "오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데"
-                        }
+                        {"오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데"}
                     </div>
                     <div className="board-detail-top-sub-box">
                         <div className="board-detail-write-info-box">
@@ -36,31 +34,18 @@ export default function BoardDetail() {
                                     backgroundImage: `url(${defaultprofileImage})`,
                                 }}
                             ></div>
-                            <div className="board-detail-writer-nickname">
-                                {"안녕하세요 나는 주코야끼"}
-                            </div>
-                            <div className="board-detail-info-divider">
-                                {"|"}
-                            </div>
-                            <div className="board-detail-write-date">
-                                {"2022. 05. 12"}
-                            </div>
+                            <div className="board-detail-writer-nickname">{"안녕하세요 나는 주코야끼"}</div>
+                            <div className="board-detail-info-divider">{"|"}</div>
+                            <div className="board-detail-write-date">{"2022. 05. 12"}</div>
                         </div>
-                        <div
-                            className="icon-button"
-                            onClick={onMoreButtonClickHandler}
-                        >
+                        <div className="icon-button" onClick={onMoreButtonClickHandler}>
                             <div className="icon more-icon"></div>
                         </div>
                         {showMore && (
                             <div className="board-detail-more-box">
-                                <div className="board-detail-update-button">
-                                    {"수정"}
-                                </div>
+                                <div className="board-detail-update-button">{"수정"}</div>
                                 <div className="divider"></div>
-                                <div className="board-detail-delete-button">
-                                    {"삭제"}
-                                </div>
+                                <div className="board-detail-delete-button">{"삭제"}</div>
                             </div>
                         )}
                     </div>
@@ -82,9 +67,7 @@ export default function BoardDetail() {
     };
     // component : 게시물 상세 하단 컴포넌트 //
     const BoardDetailBottom = () => {
-        const [favoriteList, setFavoriteList] = useState<FavoriteListItem[]>(
-            []
-        );
+        const [favoriteList, setFavoriteList] = useState<FavoriteListItem[]>([]);
         const [commentList, setCommentList] = useState<CommentListItem[]>([]);
         useEffect(() => {
             setFavoriteList(favoriteListMock);
@@ -96,18 +79,27 @@ export default function BoardDetail() {
                 <div className="board-detail-bottom-button-box">
                     <div className="board-detail-bottom-button-group">
                         <div className="icon-button">
-                            <div className="icon comment-icon"></div>
+                            <div className="icon favorite-fill-icon"></div>
                         </div>
                         <div className="board-detail-bottom-button-text">{`좋아요 ${12}`}</div>
                         <div className="icon-button">
                             <div className="icon up-light-icon"></div>
                         </div>
                     </div>
+                    <div className="board-detail-bottom-button-group">
+                        <div className="icon-button">
+                            <div className="icon comment-icon"></div>
+                        </div>
+                    </div>
+                    <div className="board-detail-bottom-button-=text">{`댓글 ${12}`}</div>
+                    <div className="icon-button">
+                        <div className="icon up-light-icon"></div>
+                    </div>
                 </div>
                 <div className="board-detail-bottom-favorite-box">
                     <div className="board-detail-bottom-favorite-container">
                         <div className="board-detail-bottom-favorite-title">
-                            {"좋아요"} <span className="emphasis">{12}</span>{" "}
+                            {"좋아요"} <span className="emphasis">{12}</span>
                         </div>
                         <div className="board-detail-bottom-favorite-contents">
                             {favoriteList.map((item) => (
@@ -119,7 +111,7 @@ export default function BoardDetail() {
                 <div className="board-detail-bottom-comment-box">
                     <div className="board-detail-bottom-comment-container">
                         <div className="board-detail-bottom-comment-title">
-                            {"댓글 "}
+                            {"댓글"}
                             <span className="emphasis">{12}</span>
                         </div>
                         <div className="board-detail-bottom-comment-list-container">
@@ -132,16 +124,11 @@ export default function BoardDetail() {
                     <div className="board-detail-bottom-comment-pagination-box">
                         <Pagination />
                     </div>
-                    <div className="board-detail-bottom-comment-input-container">
+                    <div className="board-detail-bottom-comment-input-box">
                         <div className="board-detail-bottom-comment-input-container">
-                            <textarea
-                                className="board-detail-bottom-comment-textarea"
-                                placeholder="댓글을 작성해주세요."
-                            />
+                            <textarea className="board-detail-bottom-comment-textarea" placeholder="댓글을 작성해주세요." />
                             <div className="board-detail-bottom-comment-button-box">
-                                <div className="disable-button">
-                                    {"댓글달기"}
-                                </div>
+                                <div className="disable-button">{"댓글달기"}</div>
                             </div>
                         </div>
                     </div>
