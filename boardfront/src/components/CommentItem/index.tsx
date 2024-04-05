47c43ab6ad740f56cf08c27c257131605185ff26
ktaeu <hesshes@naver.com>
2024-04-05 19:05:35 +0900

    2024-04-05 pm 19:05 msi

--- boardfront/src/components/CommentItem/index.tsx
+++ boardfront/src/components/CommentItem/index.tsx
@@ -3,13 +3,27 @@ import "./style.css";
 import { CommentListItem } from "types/interface";
 import defaultProfileImage from "assets/images/suk.png";
 
+import dayjs from "dayjs";
+
 interface Props {
     CommentListItem: CommentListItem;
 }
 // component : Comment List Item //
 export default function CommentItem({ CommentListItem }: Props) {
-    // properties //
+    // state :  properties //
     const { nickname, profileImage, wrtDttm, content } = CommentListItem;
+
+    // function : 작성일 경과 시간 함수 //
+    const getElapsedTime = () => {
+        const now = dayjs().add(9, "hour");
+        const writeTime = dayjs(wrtDttm);
+
+        const gap = now.diff(writeTime, "s");
+        if (gap < 60) return `${gap}초 전`;
+        if (gap < 3600) return `${Math.floor(gap / 60)}분 전`;
+        if (gap < 86400) return `${Math.floor(gap / 3600)}시간 전`;
+        return `${Math.floor(gap / 86400)}일 전`;
+    };
 
     // render : comment List Item render //
     return (
@@ -19,17 +33,13 @@ export default function CommentItem({ CommentListItem }: Props) {
                     <div
                         className="comment-list-item-profile-image"
                         style={{
-                            backgroundImage: `url(${
-                                profileImage != null
-                                    ? profileImage
-                                    : defaultProfileImage
-                            })`,
+                            backgroundImage: `url(${profileImage != null ? profileImage : defaultProfileImage})`,
                         }}
                     ></div>
                 </div>
                 <div className="comment-list-item-nickname">{nickname}</div>
                 <div className="comment-list-item-divider">{"|"}</div>
-                <div className="comment-list-item-time">{wrtDttm}</div>
+                <div className="comment-list-item-time">{getElapsedTime()}</div>
             </div>
             <div className="comment-list-item-main">
                 <div className="comment-list-item-content">{content}</div>
